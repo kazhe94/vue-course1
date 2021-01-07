09b3566d3f64e449851a746d61f37061f44d4611
kazhe94 <evgeniisherbakov@mail.ru>
2021-01-07 21:50:53 +0300

    some corrections

--- vue.js
+++ vue.js
@@ -6,6 +6,7 @@ const App = {
   data() {
     return {
       activeIndex: 0, // то, что позволяет определить текущий активный шаг
+      isFinished: false,
       steps: [
         {title: 'Основы', text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.'},
         {title: 'Компоненты', text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.'},
@@ -35,20 +36,21 @@ const App = {
     setActive(idx) {
       // когда нажимаем на определенный шаг
       this.activeIndex = idx
-      
+    },
+    finish() {
+      this.isFinished = true
     }
   },
   computed: {
     disableBtn() {
-      if(this.activeIndex === 0) {
-        return true
-      }
+      return this.activeIndex === 0
     },
     checkLastEl() {
-      if(this.activeIndex === this.steps.length - 1) {
-        return true
-      }
-    }
+      return this.activeIndex === this.steps.length - 1
+    },
+    printStepsText() {
+      return this.activeIndex < this.steps.length-1 ? this.steps[this.activeIndex].text : this.steps[this.steps.length-1].text
+    }    
   }
 }
 
